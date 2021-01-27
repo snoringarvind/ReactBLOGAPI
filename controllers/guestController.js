556f3444c0795859de5fb5e1dfd773b1774f4015
snoringarvind <snoringarvind@gmail.com>
2021-01-27 20:15:06 +0530

    saving

--- controllers/guestController.js
+++ controllers/guestController.js
@@ -58,7 +58,6 @@ exports.signup_post = [
     const errors = validationResult(req);
 
     const user = new User({
-      admin: true,
       fname: req.body.fname,
       lname: req.body.lname,
       email: req.body.email,
